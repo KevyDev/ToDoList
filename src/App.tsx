0bb8d9e8d4eb2d08eba951a10db875c5ae070bab
KevyDev <kevydev@outlook.com>
2023-12-31 12:04:44 -0500

    Adding MADE BY KEVYDEV, changing gap between input and button and better responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,8 @@ const allPlaceholders: string[] = [
     "Say goodbye",
     "Speak a lot",
     "Learn English",
-    "Be an airport"
+    "Be an airport",
+    "Text my mom that I wanna be an lion"
 ]
 
 const getPlaceholder = () => allPlaceholders[Math.floor(Math.random() * allPlaceholders.length)]
@@ -64,17 +65,19 @@ export default function App() {
 
     const deleteTask = (taskID: number) => setAllTasks(allTasks.filter(({id}: Task) => id !== taskID))
 
+    const autocompleteInput = () => setCurrentTask(placeholder)
+
     let completeSVGClassName: string = "fill-gray-200 hover:fill-gray-400 active:translate-y-1 h-6 cursor-pointer"
 
     return (
         <main className="fixed top-0 bottom-0 left-0 right-0 flex justify-center items-center p-4 bg-gray-100">
-            <div className="flex flex-col gap-4 bg-white p-4 rounded-xl w-full md:max-w-128 h-full md:max-h-96 shadow">
-                <section className="grid grid-cols-[1fr_3rem] gap-2 w-full">
+            <div className="flex flex-col items-center gap-4 bg-white p-4 rounded-xl w-full md:max-w-128 h-full shadow">
+                <section className="grid grid-cols-[1fr_3rem] gap-4 w-full">
                     <input
                         type="text"
                         placeholder={placeholder}
                         onChange={updateCurrentTask}
-                        onKeyDown={event => event.key === "Enter" && addNewTask()}
+                        onKeyDown={event => (event.key === "Enter" && addNewTask()) || (event.key === "Tab" && autocompleteInput())}
                         value={currentTask}
                         className="text-xl w-full h-12 rounded-xl px-4 border-bottom-4 border-violet-500 bg-gray-100"
                     />
@@ -86,16 +89,19 @@ export default function App() {
                 <ul className="flex flex-col w-full h-full overflow-auto bg-white custom-scroll">
                     {allTasks.length === 0 && <p className="text-xl w-full text-center text-gray-500 py-2">{loaded ? "You won't do anything? :(" : "Loading tasks..."}</p>}
                     {allTasks.map((task: Task) => (
-                        <li key={"task-" + task.id} className="hover:bg-gray-100 grid grid-cols-[1.5rem_1fr_1.5rem] items-center justify-center p-4 gap-4 rounded-xl">
+                        <li key={"task-" + task.id} className="hover:bg-gray-100 grid grid-cols-[1.5rem_1fr_1.5rem] w-full items-center justify-center p-4 gap-4 rounded-xl">
                             {task.completed ?
                                 <SvgCircleCheck className={completeSVGClassName} onClick={() => completeTask(task.id)} /> :
                                 <SvgCircle className={completeSVGClassName} onClick={() => completeTask(task.id)} />
                             }
-                            <span className={"text-xl" + (task.completed ? " text-gray-500 line-through" : "")}>{task.text}</span>
+                            <span className={"text-xl " + (task.completed ? "text-gray-500 line-through" : "text-black")}>{task.text}</span>
                             <SvgTrashCan className="fill-red-500 hover:fill-red-400 active:translate-y-1 h-6 cursor-pointer" onClick={() => deleteTask(task.id)} />
                         </li>
                     ))}
                 </ul>
+                <a className="text-base text-gray-500 hover:underline" href="https://kevydev.github.io/portfolio/" target="_blank">
+                    <p>© Made and designed by KevyDev.</p>
+                </a>
             </div>
         </main>
     )
